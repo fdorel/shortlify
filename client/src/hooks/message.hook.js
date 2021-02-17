32c028848e6014074d470a3b0931bbdd1bcc9e6f
Dorel Furdui <dorelfurdui@gmail.com>
2021-02-16 20:34:16 -0500

    Update message.hook.js

--- client/src/hooks/message.hook.js
+++ client/src/hooks/message.hook.js
@@ -3,7 +3,7 @@ import {useCallback} from 'react';
 export const useMessage = () => {
   return useCallback (text => {
       if (window.M && text) {
-        window.M.toast({ html: text, user._id})
+        window.M.toast({ html: text })
       }
   }, [])
 }
