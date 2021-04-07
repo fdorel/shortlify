680aa6c126cb7f518dcd523807c91c2a5f4ab195
Dorel Furdui <dorelfurdui@gmail.com>
2021-04-06 23:26:45 -0400

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import {createContext} from 'react';
 
 // a simple function that by default doesn't do anything at all
-function noop1() {}
+function noop() {}
 
 // the AuthContext will send parameters not through the tree-structure, but through the context. That's why in 'App.js' we gonna wrap the <Router> with the <AuthContext>
 export const AuthContext = createContext({
